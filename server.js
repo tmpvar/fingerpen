6fee8daad236dc8fbce7774cc7a13944d9b74ce1
Elijah Insua <tmpvar@gmail.com>
2012-07-03 00:05:14 -0700

    start using hook.io

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var SerialPort = require('serialport').SerialPort;
-var sp;
-try {
-  sp = new SerialPort('/dev/tty.usbmodemfd121', { baudrate: 9600 });
-} catch (e) {
-  console.log('could not open serial port')
-}
+var hook = require('hook.io').createHook({
+  name : 'fingerpen',
+  silent: true
+});
+
+hook.start();
+
 var path = require("path"),
     strata = require("strata");
 
@@ -16,23 +16,21 @@ var app = new strata.Builder;
 app.use(strata.commonLogger);
 app.use(strata.static, root, ['index.html']);
 
-var io = require('socket.io').listen(strata.run(app));
+hook.on('hook::ready', function() {
+  var io = require('socket.io').listen(strata.run(app));
+  io.sockets.on('connection', function (socket) {
+    socket.on('movement', function (data) {
 
-sp.write('G90\n');
-
-io.sockets.on('connection', function (socket) {
-  socket.on('movement', function (data) {
-console.log(data);
-    if (data.x > 370) {
-      return;
-    }
-
-    sp.write('G90\n');
-
-    sp.write([
-      'G1',
-      'X' + data.x,
-      'F800'
-    ].join(' ') + '\n');
+      hook.emit("gcode", {
+        name : 'fingerpen gcode packet',
+        gcode : [
+          [
+            'G1',
+            'X' + data.x,
+            'F800'
+          ].join(' ')
+          ]
+      });
+    });
   });
 });
